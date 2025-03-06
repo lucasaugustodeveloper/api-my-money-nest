da6e5476bec1e5846caea2ba1a77f77c5861e449
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2025-03-06 08:42:11 -0300

    ♻️ Change return for initial route

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, HelloWorld } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): HelloWorld {
     return this.appService.getHello();
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+export type HelloWorld = {
+  version: string;
+  message: string;
+};
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): HelloWorld {
+    return { version: '0.1.0', message: 'Hello, World!' };
   }
 }
